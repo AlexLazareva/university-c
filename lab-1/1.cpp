cf1706826b4c5c1aae15abc5233771c04a0cba82
Alex Lazareva <alex.lazareva25@yandex.ru>
2020-01-09 23:02:26 +0300

    complete 1 laboratory work

---
+++ lab-1/1.cpp
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define PROMPT ':'
+
+main() {
+    float a,b,rez;
+    char oper;
+
+    while (putch(PROMPT), scanf("%f%c%f", &a, &oper, &b) !=EOF)
+    {
+        switch (oper)
+        {
+        case '+':
+            rez=a+b;
+            break;
+
+        case '-':
+            rez=a-b;
+            break;
+        
+        case '*':
+            rez=a*b;
+            break;
+        case '/':
+            rez=a/b;
+            break;
+
+        default:
+            printf("Ошибка /n");
+            exit(1);
+        }
+        printf("Результат:%f\n",rez);
+    }
+    
+}
