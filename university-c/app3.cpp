afe0a6b04b0de9edc694b03c8d531b7674dd4105
Alex Lazareva <alex.lazareva25@yandex.ru>
2020-01-16 18:15:48 +0300

    changed 3rd laboratory

---
+++ university-c/app3.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+#include<stdio.h>
+#include<conio.h>
+
+//#define size 10
+using namespace std;
+
+int main (){
+
+const int size = 10;
+ double arr[size];
+ double sum;
+ double min;
+ int i,j;
+
+// формирование массива
+ for (i = 0; i < size; i++){
+    cout << "arr[" << i << "]=";
+    cin >> arr[i];
+ }
+
+// эхо-печать массива
+cout << "Entered array is" << endl;
+
+ for (i=0; i<size; i++){
+    cout << i << " " << setprecision(3) << arr[i] << endl;
+ }
+
+// поиск минимального элемента
+    for (i=0, min=arr[0]; i<size; i++)
+        if (arr[i]<min)
+            min = arr[i];
+
+// поиск первого положительного элемента
+    for (i=0; i<size && arr[i]<=0; i++);
+    sum = 0; // если нет вообще положительных элементов
+
+//поиск последнего положительного элемента
+    if ((i<size-2) && (arr[i]>0)) // а есть ли смысл?
+    {
+        for (j=size-1; j>=0 && arr[j]<=0; j--);
+        if ((arr[j]>0) && (--j>i))
+            while (j>i){
+// сумма элементов между первым и последним положительным
+                sum+=arr[j--];
+            }
+    }
+
+    for (i=j=size-1; i>=0;--i){
+        if (arr[i]!=0)
+        {
+            if (i!=j)
+                arr[j]=arr[i];
+            --j;
+        }
+    }
+    for (i=0;i<=j;i++) // заполним начало нулями
+        arr[i]=0;
+
+// печать нового массива
+    cout << endl;
+    cout << "New array" << endl;
+
+ for (i=0; i<size; i++){
+  cout << i << " " << setprecision(3) << arr[i] << endl;
+ }
+    cout << "Minimal element = " << setprecision(3) << min << endl;
+    cout << "Summary between the first and the last element = ";
+    cout << setprecision(3);
+    cout << sum << endl;
+	system("pause");
+    return 0;
+}
