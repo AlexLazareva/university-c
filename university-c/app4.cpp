29691f8a4817355c4feb64beca76cca46a2c4848
Alex Lazareva <alex.lazareva25@yandex.ru>
2020-01-20 22:50:40 +0300

    created structures in 4th laboratory

--- university-c/app4.cpp
+++ university-c/app4.cpp
@@ -1,24 +1,43 @@
 #include <stdio.h>
-#include <math.h>
 #include <iostream>
 #include <conio.h>
 
 using namespace std;
+
 struct Team {
         char * name[20];
         char * city[20];
-        char * instructor;
-        char * rate;
-    } team;
+        char * instructor[20];
+        char * rate[3];
+    } team[10]; // создали список из 10 команд
+
+struct search {
+    char * isNull[15];
+    char * d[15];
+} search[2];
+
+int i = 0,
+    mount = 0;
+
+// ввод данных команды
+void inputData() {
+    i = mount;
+
+    cout << "name: ";
+    cin >> team[i].name;
+    cout << "city: ";
+    cin >> team[i].city;
+    cout << "instructor name: ";
+    cin >> team[i].instructor;
+    cout << "rate: ";
+    cin >> team[i].rate;
+
+    system("pause");
+    mount++;
+}
 
 int main() {
-    Team team[10]; // создали список команд
-    cout << "name:";
-    cin >> team.name;
-    cout << "city:";
-    cin >> team.city;
-    cout << "instructor name:";
-    cin >> instructor.name;
+
 
     getch();
     return 0;
