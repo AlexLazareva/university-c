e6d492e6bf99bb6ac29cdedc8c98d5e4b4585937
Alex Lazareva <alex.lazareva25@yandex.ru>
2020-01-20 11:17:14 +0300

    fixed input in 3th laboratory

--- university-c/app3.cpp
+++ university-c/app3.cpp
@@ -17,57 +17,57 @@ const int size = 10;
  double value;
 
 // формирование массива
- for (int i = 0; i < size; i++){
-    cout << "arr[" << i << "]=";
-    cin >> arr[i];
+ for (int n = 0; n < size; n++){
+    cout << "arr[" << n << "]=";
+    cin >> arr[n];
  }
 
 // эхо-печать массива
 cout << "Entered array is" << endl;
 
- for (int i=0; i < size; i++){
-    cout << i << " " << setprecision(3) << arr[i] << endl;
+ for (int n = 0; n < size; n++){
+    cout << n << " " << arr[n] << endl;
  }
 
 // поиск минимального элемента
-    for (int i=0, min=arr[0]; i<size; i++)
-        if (arr[i] < min)
-            min = arr[i];
+    for (int n = 0, min = arr[0]; n < size; n++)
+        if (arr[n] < min)
+            min = arr[n];
 
 // поиск первого положительного элемента
-    for (int i=0; i < size && arr[i] <= 0; i++);
+    for (int n = 0; n < size && arr[n] <= 0; n++);
     sum = 0; // если нет вообще положительных элементов
 
 //поиск последнего положительного элемента
-    if ((i<size-2) && (arr[i]>0)) {
-        for (j=size-1; j>=0 && arr[j]<=0; j--);
-        if ((arr[j]>0) && (--j>i))
-            while (j>i){
+    if ((i < size - 2) && (arr[i] > 0)) {
+        for (j = size-1; j >= 0 && arr[j] <= 0; j--);
+        if ((arr[j] > 0) && (--j > i))
+            while (j > i){
 // сумма элементов между первым и последним положительным
-                sum+=arr[j--];
+                sum += arr[j--];
             }
     }
 
-    for (i=j=size-1; i>=0;--i){
-        if (arr[i]!=0){
-            if (i!=j)
-                arr[j]=arr[i];
+    for (i = j = size - 1; i >= 0; --i){
+        if (arr[i]!= 0){
+            if (i != j)
+                arr[j] = arr[i];
             --j;
         }
     }
-    for (int i=0;i<=j;i++) // заполним начало нул€ми
-        arr[i]=0;
+    for (i = 0; i <= j; i++) // заполним начало нул€ми
+        arr[i] = 0;
 
 // печать нового массива
     cout << endl;
     cout << "New array" << endl;
 
- for (i=0; i<size; i++){
-  cout << i << " " << setprecision(3) << arr[i] << endl;
+ for (i = 0; i < size; i++){
+  cout << i << " " << arr[i] << endl;
  }
-    cout << "Minimal element = " << setprecision(3) << min << endl;
+    cout << "Minimal element = " << min << endl;
     cout << "Summary between the first and the last element = ";
-    cout << setprecision(3);
+    //cout << setprecision(3);
     cout << sum << endl;
 	system("pause");
     return 0;
