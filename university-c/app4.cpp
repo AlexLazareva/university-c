670921b384b2a306883dc3a0fb391f1cee796520
Alex Lazareva <alex.lazareva25@yandex.ru>
2020-01-21 12:00:06 +0300

    edited structures in 4th laboratory and added functions

--- university-c/app4.cpp
+++ university-c/app4.cpp
@@ -1,44 +1,124 @@
-#include <stdio.h>
-#include <iostream>
-#include <conio.h>
+#include "stdio.h"
+#include "string.h"
+#include "conio.h"
+#include "windows.h"
 
-using namespace std;
-
-struct Team {
-        char * name[20];
-        char * city[20];
-        char * instructor[20];
-        char * rate[3];
-    } team[10]; // создали список из 10 команд
+struct team {
+    char name[15], city[15], teamlead[15], rate[15];
+} a[100];
 
 struct search {
-    char * isNull[15];
-    char * d[15];
+    char notValue[15], d[15];
 } search[2];
 
 int i = 0,
     mount = 0;
 
-// ввод данных команды
-void inputData() {
-    i = mount;
+void addTeam() {
+        i = mount;
 
-    cout << "name: ";
-    cin >> team[i].name;
-    cout << "city: ";
-    cin >> team[i].city;
-    cout << "instructor name: ";
-    cin >> team[i].instructor;
-    cout << "rate: ";
-    cin >> team[i].rate;
+        printf("Enter name of team: ");
+        scanf("%s", &a[i].name);
 
-    system("pause");
-    mount++;
+        printf("Enter city: ");
+        scanf("%s", &a[i].city);
+
+        printf("Enter teamlead: ");
+        scanf("%s", &a[i].teamlead);
+
+        printf("Enter rating value: ");
+        scanf("%s", &a[i].rate);
+
+        {
+            system("cls");
+            printf("\n Done! \n");
+            mount++;
+        }
 }
 
-int main() {
+void showTeams(){
+    for (int i = 0; i <= mount - 1; i++) {
+        if (a[i].name != search[0].notValue)
+            printf("%s  ", a[i].name);
+            printf("%s  ", a[i].city);
+            printf("%s  ", a[i].teamlead);
+            printf("%s  \n", a[i].rate);
+        }
+}
 
+void searchTeamByCity() {
+        printf("Enter team's city for searching: ");
+        scanf("%s", &search[0].d);
+
+        for (i = 0; i <= mount-1; i++){
+          if (!strcmp(a[i].city, search[0].d)){
+                printf("%s  ", a[i].name);
+                printf("%s  \n", a[i].city);
+                printf("%s  ", a[i].teamlead);
+                printf("%s  \n", a[i].rate);
+           }
+        }
+}
+
+void edit() {
+        printf("Enter team's name for editing': \n");
+        scanf("%s",&search[0].d);
+
+        for (i = 0; i <= mount - 1; i++){
+          if (!strcmp(a[i].name, search[0].d)){
+                printf("Enter the new team's name: ");
+                scanf("%s", a[i].name);
+
+                printf("Enter the new team's city: ");
+                scanf("%s", a[i].city);
+
+                printf("Enter the new team's teamlead: ");
+                scanf("%s", a[i].teamlead);
+
+                printf("Enter the new team's rate: ");
+                scanf("%s", a[i].rate);
+
+                printf("Success!\n");
+                break;
+            }
+        }
+}
+void removeTeam() {
+        printf("Enter the team name for removing from teams's list: ");
+        scanf("%s", &search[0].d);
+
+    for (i = 0; i <= mount - 1; i++) {
+        if (!strcmp(a[i].name, search[0].d)){
+            a[i].name == search[0].notValue;
+        }
+
+        {
+            system("cls");
+            printf(" \n Removed success! \n");
+        }
+    }
+}
+
+int main(){
+    int action = 1;
+
+    while(action) {
+        printf("\n MENU: \n 1 - Create new team' 2 - Show all teams' 3 - Edit' 4 - Remove' 5 - Search team by City, 0 - Exit\n");
+        printf("\n Select action: ");
+        scanf("%d", &action);
+
+        switch (action) {
+                case 0: printf("Goodbay!\n"); break;
+                case 1: addTeam(); break;
+                case 2: showTeams(); break;
+                case 3: edit(); break;
+                case 4: removeTeam(); break;
+                case 5: searchTeamByCity(); break;
+            default:
+                printf("There is not such team\n");
+        }
+    }
 
     getch();
     return 0;
-};
+}
